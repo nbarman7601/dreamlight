2b189b80b7c1c73db1e5d5403596045921c729dd
Nandan <nbarman760@gmail.com>
2024-11-12 18:38:07 +0530

    first commit

--- dist/config/config.development.js
+++ dist/config/config.development.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = void 0;
 exports.environment = {
     production: false,
-    DB_URL: `mongodb+srv://prasenjit2342:Barman%402342@cluster0.7coju.mongodb.net/swapnerghar?retryWrites=true&w=majority`,
+    DB_URL: process.env.MONGO_URI,
     APP_SECRET_KEY: "swapnoghor3016"
     // Add other development-specific configuration here
 };
